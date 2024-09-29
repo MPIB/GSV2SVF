b33aa698b8b3c7a61ccccfb255d24a8867ed4c37
Michael Krause <krause@mpib-berlin.mpg.de>
2024-09-30 00:03:14 +0200

    Do not claim LD_LIBRARY_PATH

--- docker/env.sh
+++ docker/env.sh
@@ -1,4 +1,4 @@
 [ "$CAFFE_BLEND" != "cpu" ] && [ "$CAFFE_BLEND" != "gpu" ] && echo "CAFFE_BLEND=[gpu|cpu] environment is required" && exit 1
 
 export PYTHONPATH=/usr/local/caffe-$CAFFE_BLEND/python
-export LD_LIBRARY_PATH=/usr/local/caffe-$CAFFE_BLEND/lib:/usr/local/cudnn/lib64
+export LD_LIBRARY_PATH=/usr/local/caffe-$CAFFE_BLEND/lib:/usr/local/cudnn/lib64:$LD_LIBRARY_PATH
